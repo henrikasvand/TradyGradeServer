9ae4b274e7a5715ddb4aa2696fab06b3714674c4
Henri <henri.kasvand@gmail.com>
2020-04-20 15:47:44 +0300

    new build without localhost 4000 routes

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.25431e2af27285f2731646778045a2c1.js"
+  "/precache-manifest.8e5938f8dbaa8799913a98c298000fa3.js"
 );
 
 self.addEventListener('message', (event) => {
